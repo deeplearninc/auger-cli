7473a80c0ffa870e0b2b3bdbf80da60018b92916
knobotz <30328334+knobotz@users.noreply.github.com>
2019-03-06 10:50:09 -0800

    Adding pipenv support and update circlci for 3.6/3.7 python support

--- cluster_task_info.py
+++
@@ -1,3 +0,0 @@
-def get_cluster_task_info():
-    return 'auger_ml.tasks_queue.tasks.list_project_files_task', {"augerInfo": {"experiment_id": None, "dataset_manifest_id": None}}
-
--- setup.py
+++ setup.py
@@ -16,18 +16,12 @@ here = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt')) as f:
-    REQUIRES = f.readlines()
-
-with open(path.join(here, 'test-requirements.txt')) as f:
-    TESTS_REQUIRES = f.readlines()
-
 setup(
     name='auger-cli',
     version=VERSION,
     url='https://github.com/deeplearninc/auger-cli',
     license='MIT',
-    author='DeepLearn',
+    author='Auger AI',
     description='Command line tool for the Auger AI platform.',
     long_description=long_description,
     packages=find_packages(exclude=['docs', 'tests']),
@@ -35,18 +29,34 @@ setup(
     zip_safe=False,
     platforms='any',
     test_suite='tests',
+    python_requires='>=3',
+    keywords='augerai auger ai machine learning automl deeplearn api sdk',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        "Intended Audience :: System Administrators",
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3 :: Only"
     ],
-    install_requires=REQUIRES,
-    tests_require=TESTS_REQUIRES,
+    install_requires=[
+        'click>=6.7',
+        'click-spinner>=0.1',
+        'coreapi==2.1.1',
+        'coreapi-cli==1.0.6',
+        'openapi-codec==1.2.1',
+        'ruamel.yaml'
+    ],
+    setup_requires=[
+        'flake8==3.4.1'
+    ],
+    tests_require=[
+        'coverage==4.4.1',
+        'vcrpy==1.11.1'
+    ],
     entry_points={
         'console_scripts': [
             'auger=auger_cli.cli:cli',
