962caff69e9f89147db766004b492369529b95bb
Igor Flint <igor@dplrn.com>
2019-05-27 19:32:37 +0300

    -pager turned off currently +fixed pagination request issue +filter by pod_name +filter by text regexp +filter by tracebacks

--- auger_cli/logging/stream.py
+++ auger_cli/logging/stream.py
@@ -8,11 +8,21 @@ class BaseStream():
         raise NotImplemented
 
 class ApiStream(BaseStream):
-    def __init__(self, client):
+    def __init__(self, client, limit=10000):
         self.__client = client
+        self.__limit = limit
 
     def get_stream(self, project_id):
-        return self.__client.call_hub_api_ex('get_pod_logs', params={'project_id': project_id})
+        # return self.__client.call_hub_api_ex('get_pod_logs', params={'project_id': project_id})
+        offset = 0
+        while True:
+            page = self.__client.call_hub_api_ex('get_pod_logs', params={'project_id': project_id, 'limit': self.__limit, 'offset': offset})
+            yield page
+            pagination = page['meta']['pagination']
+            offset += self.__limit
+            if pagination['total'] <= offset:
+                break
+
 
 class WebSocketStream(BaseStream):
     # TODO: implement long-live websocket connection reader
